8b85a3f4753bd71b0a1bf743798041948e469d9c
oscar-PC\oscar <oscar4m2@gmail.com>
2018-12-25 22:25:33 -0500

    Initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    Rigidbody rigidbody;
+
+
+	// Use this for initialization
+	void Start () {
+
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        RespondToMoveInput();
+	}
+
+    private void RespondToMoveInput()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody.AddRelativeForce(Vector3.forward * Time.deltaTime);
+        }
+    }
+}
