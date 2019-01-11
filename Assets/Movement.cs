ef16b3769808e0d02045254091dcb79b6b695674
oscar-PC\oscar <oscar4m2@gmail.com>
2019-01-10 21:17:11 -0500

    Added movement to ball

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,7 +3,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        //checks whether arrow or WASD buttons are used.
+        if (Input.GetKey(KeyCode.A | KeyCode.W | KeyCode.S | KeyCode.D))
+        {
+            WASDMovement();
+        }
+        ArrowMovement();
+    }
+
+
+    //gets input from Arrow buttons and moves sphere
+    private void ArrowMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void WASDMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
+
+
+//OLD CODE for movement
+/**public class Movement : MonoBehaviour {
 
     Rigidbody rigidbody;
 
@@ -25,7 +75,9 @@ public class Movement : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            rigidbody.AddRelativeForce(Vector3.forward * Time.deltaTime);
+            rigidbody.AddForce(Vector3.forward * Time.deltaTime);
+            //rigidbody.AddRelativeForce(Vector3.forward * Time.deltaTime);
         }
     }
 }
+**/
