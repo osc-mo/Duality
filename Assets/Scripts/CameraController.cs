5b4b8ab53ed14d43b223f87f0735b21c86e321bf
osc-mo <oscar4m2@gmail.com>
2019-11-30 22:37:23 -0500

    Using camera rig and rolling ball from Unity standards assets store. Reconfigured player movement and camera movement.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public GameObject player;
-    public float damping = 1;
-    private Vector3 offset;
+    
+    [SerializeField] float turnSpeed = 3.0f;
+    private Vector3 desiredCam;
+    
+    
+
 
     void Start()
     {
-        offset = player.transform.position - transform.position;
+        desiredCam = (player.transform.position + transform.position);
+
+
+
     }
 
     void LateUpdate()
     {
-        float currentAngle = transform.eulerAngles.y;
-        float desiredAngle = player.transform.eulerAngles.y;
-        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
-        Quaternion rotation = Quaternion.Euler(0, angle, 0);
-        transform.position = player.transform.position - (rotation * offset);
-        transform.LookAt(player.transform);
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3 (direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+        transform.position = player.transform.position+desiredCam;//desiredCam*Time.deltaTime;
+        //momentum = player.GetComponent<Rigidbody>().velocity.normalized;
+        //  Vector3 desiredCamPos = (player.transform.position - offset);
+        //transform.position = Vector3.SmoothDamp(transform.position,desiredCamPos, ref velocity, smoothing *Time.deltaTime) *desiredCamPos;
+
+       
+       /* Vector3 offset = rotates.eulerAngles;//+ momentum;
+        transform.position = player.transform.position * Time.deltaTime;
+        transform.LookAt(transform.rotation);*/
+
 
     }
 }
